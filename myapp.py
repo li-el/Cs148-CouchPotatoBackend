917cb3fc4058468038fb18a21ce6e3e9f2eeb566
li-el <li-el@ucsb.edu>
2020-11-29 02:48:29 +0000

    debug

--- myapp.py
+++ myapp.py
@@ -157,7 +157,7 @@ def passRoom():
     if room:
         try:
             db = firebase.database()
-            response["ROOMKEY"] = db.child(user).push(room)
+            response["ROOMKEY"] = db.child(user).push(room)["names"]
             #db.push(room)
             response["MESSAGE"]= "Room Successfully saved"
             status = 200
