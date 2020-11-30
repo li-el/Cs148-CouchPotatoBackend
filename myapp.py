2f37d771936038070733779b4615665554c4c8f0
li-el <li-el@ucsb.edu>
2020-11-30 03:51:33 +0000

    debugging

--- myapp.py
+++ myapp.py
@@ -192,7 +192,7 @@ def listRooms():
         if user:
             try:
                 db = firebase.database()
-                response["LIST"] = db.child(user).shallow().get().key()
+                response["LIST"] = db.child(user).shallow().get()[0]
                 response["MESSAGE"]= "List of Room Keys returned"
                 status = 200
             except Exception as e:
