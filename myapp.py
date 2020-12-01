0bce1ee8ed8539b64eef50409f2c584912a7a27a
li-el <li-el@ucsb.edu>
2020-12-01 00:56:33 +0000

    debug

--- myapp.py
+++ myapp.py
@@ -198,7 +198,7 @@ def listRooms():
                 response["NAME"] = []
                 for room in lists.each():
                     response["LIST"].append(room.key())
-                    response["NAME"].append(room.val()["name"])
+                    response["NAME"].append(room.val())
                 response["MESSAGE"]= "List of Room Keys returned"
                 status = 200
             except Exception as e:
